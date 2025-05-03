daeecae84219d328553c44e2e32fd2409ecec846
Muhammad Azqi Madani Ardan <muhammadazqi098@gmail.com>
2025-05-03 12:58:51 +0700

    update layout

--- src/Components/InfiniteScrollLanguage.jsx
+++ src/Components/InfiniteScrollLanguage.jsx
@@ -1,5 +1,5 @@
 import { motion, useAnimation } from "framer-motion";
-import { useEffect, useMemo, useRef } from "react";
+import { useEffect, useState } from "react";
 import {
   SiJavascript,
   SiPython,
@@ -18,50 +18,44 @@ import {
 
 // Main Component
 const InfiniteScrollLanguages = () => {
-  // Memoizing the languages array inside the component
-  const languages = useMemo(
-    () => [
-      { name: "JavaScript", icon: <SiJavascript size={40} /> },
-      { name: "Python", icon: <SiPython size={40} /> },
-      { name: "PHP", icon: <SiPhp size={40} /> },
-      { name: "React", icon: <SiReact size={40} /> },
-      { name: "Tailwindcss", icon: <SiTailwindcss size={40} /> },
-      { name: "Bootstrap", icon: <SiBootstrap size={40} /> },
-      { name: "Unity", icon: <SiUnity size={40} /> },
-      { name: "Html5", icon: <SiHtml5 size={40} /> },
-      { name: "Css3", icon: <SiCss3 size={40} /> },
-      { name: "Laravel", icon: <SiLaravel size={40} /> },
-      { name: "Postgresql", icon: <SiPostgresql size={40} /> },
-      { name: "Nodedotjs", icon: <SiNodedotjs size={40} /> },
-    ],
-    []
-  );
+  const [isAnimating, setIsAnimating] = useState(false);
+  
+  const languages = [
+    { name: "JavaScript", icon: <SiJavascript size={40} /> },
+    { name: "Python", icon: <SiPython size={40} /> },
+    { name: "PHP", icon: <SiPhp size={40} /> },
+    { name: "React", icon: <SiReact size={40} /> },
+    { name: "Tailwindcss", icon: <SiTailwindcss size={40} /> },
+    { name: "Bootstrap", icon: <SiBootstrap size={40} /> },
+    { name: "Unity", icon: <SiUnity size={40} /> },
+    { name: "Html5", icon: <SiHtml5 size={40} /> },
+    { name: "Css3", icon: <SiCss3 size={40} /> },
+    { name: "Laravel", icon: <SiLaravel size={40} /> },
+    { name: "Postgresql", icon: <SiPostgresql size={40} /> },
+    { name: "Nodedotjs", icon: <SiNodedotjs size={40} /> },
+  ];
 
   const controls = useAnimation();
-  const isComponentVisible = useRef(true);
 
   useEffect(() => {
-    const animate = async () => {
-      while (isComponentVisible.current) {
-        await controls.start({
-          x: "-100%", 
-          transition: {
-            duration: 30, 
-            ease: "linear",
-          },
-        });
+    const animate = () => {
+      controls.start({
+        x: "-100%",
+        transition: {
+          duration: 30, 
+          ease: "linear",
+        },
+      }).then(() => {
         controls.set({ x: 0 }); 
-      }
+        setIsAnimating(false);  
+      });
     };
 
-    if (isComponentVisible.current) {
+    if (!isAnimating) {
+      setIsAnimating(true);
       animate();
     }
-
-    return () => {
-      isComponentVisible.current = false;
-    };
-  }, [controls]);
+  }, [controls, isAnimating]);
 
   return (
     <div className="overflow-hidden w-full bg-transparent py-4">
